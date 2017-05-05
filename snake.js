f8922cbb911004e3da7a06fe627e5392a37fe132
Arlo Shefte-Jacobs <arlo.sj3@gmail.com>
2017-05-05 13:16:16 -0600

    FISRT

---
+++ snake.js
@@ -0,0 +1,5 @@
+console.log()
+
+curl --get --include 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/jokes/random' \
+  -H 'X-Mashape-Key: cfe79eKrY4mshktnHDES4fxFhHDlp1F32RdjsnQdJQwCi8aEZs' \
+  -H 'Accept: application/json'
